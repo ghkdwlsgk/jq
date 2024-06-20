72cd0e5f8686e19b8f6b3513ed4f96301406fe1f
ghkdwlsgk <hjh060424@gmail.com>
2024-06-20 14:03:36 +0900

    오,왼,아래,위 벽설치

--- main.js
+++ main.js
@@ -12,10 +12,32 @@ const render = Reader.create({
     options: {
         wireframes : false,
         background : '#F7F4C8',
-        width : 420,
+        width : 620,
         height : 850,
     }
 })
 
+const world = engine.world;
+const leftWall = Bodies.rectangle(15,395, 30,790,{
+    isStatic : true,
+    render : {fillStyle:'#E6B143'}
+})
+
+const rightWall = Bodies.rectangle(605,395, 30,790,{
+    isStatic : true,
+    render : {fillStyle:'#E6B143'}
+})
+
+const gorund = Bodies.rectangle(310,820, 620,60,{
+    isStatic : true,
+    render : {fillStyle:'#E6B143'}
+})
+ 
+const topLine = Bodies.rectangle(310,150, 620,3,{
+    isStatic : true,
+    render : {fillStyle:'#E6B143'}
+})
+
+World.add(world, [leftWall,rightWall,gorund,topLine]);
 Reader.run(render);
 Runner.run(engine);
