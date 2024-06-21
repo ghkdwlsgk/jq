b6e3abafa5ff1740f9e131fb9d53ff9f0ec2600a
ghkdwlsgk <hjh060424@gmail.com>
2024-06-21 09:17:48 +0900

    고일 추가

--- main.js
+++ main.js
@@ -1,3 +1,5 @@
+import {FRUITS} from "./fruits.js"
+
 let Engine = Matter.Engine,
     Reader = Matter .Render,
     Runner = Matter .Runner,
@@ -35,9 +37,41 @@ const gorund = Bodies.rectangle(310,820, 620,60,{
  
 const topLine = Bodies.rectangle(310,150, 620,3,{
     isStatic : true,
+    isSensor : true,
     render : {fillStyle:'#E6B143'}
 })
 
 World.add(world, [leftWall,rightWall,gorund,topLine]);
+
 Reader.run(render);
 Runner.run(engine);
+
+let currentBody = null;
+let currentFruit = null;
+
+//과일 떨구기
+function addFruit(){
+
+    const index = Math.floor(Math.random()*5);
+
+    const fruit =FRUITS[index]
+
+    const body = Bodies.circle(300, 50, fruit.radius,{
+        
+        index: index,
+        isSleeping : true,
+
+        render:{
+            sprite : { texture: `${fruit.name}.png`},
+
+        },
+        restitution : 1
+    });
+
+    currentBody = body;
+    currentFruit = fruit;
+
+    World.add(world,body);
+}
+
+addFruit();
